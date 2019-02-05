01a54ff6ff1bff13daa167eca0b6514e093c36f0
patrickcarmo <patrick.a.carmo@gmail.com>
2019-02-05 23:04:06 +0000

    Store

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import './App.css';
 import Header from './common/Header';
 import Home from './home';
 import CreateList from './create-list';
+import store from './store';
 
 const theme = createMuiTheme({
 	typography: {
@@ -23,18 +25,20 @@ const theme = createMuiTheme({
 
 class App extends Component {
   render() {
-	return (
-		<MuiThemeProvider theme={theme}>
-			<Router>
-				<div>
-					<Header />
-					<Switch>
-						<Route exact path='/' component={Home} />
-						<Route path='/lista' component={CreateList} />
-					</Switch>
-				</div>
-			</Router>
-		</MuiThemeProvider>
+	  return (
+		  <Provider store={store}>
+			<MuiThemeProvider theme={theme}>
+				<Router>
+					<div>
+						<Header />
+						<Switch>
+							<Route exact path='/' component={Home} />
+							<Route path='/lista' component={CreateList} />
+						</Switch>
+					</div>
+				</Router>
+			</MuiThemeProvider>	  
+		</Provider>		
     );
   }
 }
